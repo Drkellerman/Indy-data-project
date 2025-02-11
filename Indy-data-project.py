604c9332c8c0b354983aa542da6bd07a5da53812
Devin Kellerman <devinrkellerman@yahoo.com>
2025-02-11 15:12:04 -0500

    Create Indy-data-project.py

---
+++ Indy-data-project.py
@@ -0,0 +1,79 @@
+# Objective- Discover definitive trend in numeric population change and out-migration of people over time for Marion County, Indiana over the course of time, 
+# given change in unemployment rate.
+# Three sources of data used to 
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+Indy_EconData= pd.read_csv("Indy_EconData.csv")
+
+pd.set_option('display.max_rows', None)
+Indy_EconData
+pd.read_csv ("Indy_EconData.csv")
+
+Indy_PopData2023 =pd.read_csv("Indy_PopData2023.csv")
+
+pd.set_option('display.max_rows', None)
+Indy_PopData2023
+pd.read_csv ("Indy_PopData2023.csv")
+
+Indy_PopData2022 =pd.read_csv("Indy_PopData2022.csv")
+
+pd.set_option('display.max_rows', None)
+Indy_PopData2022
+pd.read_csv ("Indy_PopData2022.csv")
+
+Indy_PopData2021 =pd.read_csv("Indy_PopData2021.csv")
+
+pd.set_option('display.max_rows', None)
+Indy_PopData2021
+pd.read_csv ("Indy_PopData2021.csv")
+
+Indy_PopData2020 =pd.read_csv("Indy_PopData2020.csv")
+
+pd.set_option('display.max_rows', None)
+Indy_PopData2020
+pd.read_csv ("Indy_PopData2020.csv")
+
+Indy_PopData2019 =pd.read_csv("Indy_PopData2019.csv")
+
+pd.set_option('display.max_rows', None)
+Indy_PopData2019
+pd.read_csv ("Indy_PopData2019.csv")
+
+Indy_PopData2018 =pd.read_csv("Indy_PopData2018.csv")
+
+pd.set_option('display.max_rows', None)
+Indy_PopData2018
+pd.read_csv ("Indy_PopData2018.csv")
+
+Indy_PopData2017 =pd.read_csv("Indy_PopData2017.csv")
+
+pd.set_option('display.max_rows', None)
+Indy_PopData2017
+pd.read_csv ("Indy_PopData2017.csv")
+
+Indy_PopData2016 =pd.read_csv("Indy_PopData2016.csv")
+
+pd.set_option('display.max_rows', None)
+Indy_PopData2016
+pd.read_csv ("Indy_PopData2016.csv")
+
+Indy_PopData2015 =pd.read_csv("Indy_PopData2015.csv")
+
+pd.set_option('display.max_rows', None)
+Indy_PopData2015
+pd.read_csv ("Indy_PopData2015.csv")
+
+Indy_PopMigration =pd.read_csv("Indy_PopMigration.csv")
+
+pd.set_option('display.max_rows', None)
+Indy_PopMigration
+pd.read_csv ("Indy_PopMigration.csv")
+
+
+
+
+
+
